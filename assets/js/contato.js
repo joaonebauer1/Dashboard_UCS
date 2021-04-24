62bdeaf7a061457fd06fe8603156df5ddc6b9370
João Junior  Nebauer Schneider <55754251+joaonebauer1@users.noreply.github.com>
2021-04-24 18:37:26 -0300

    atualização
    Mudança de alguns trechos, ainda falta botão contado funcionar

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -7,3 +7,52 @@ $(document).ready(function() {
         $('#overlay').fadeOut(300);
     });
 });
+
+const init = () => {
+
+    const errorHandler = () => {
+        submitButton.classList.remove('loading');
+        submitButton.classList.remove('success');
+        submitButton.classList.add('error');
+        submitButton.textContent = "Erro, tente novamente";
+    }
+
+    const successHandler = () => {
+        submitButton.classList.remove('loading');
+        submitButton.classList.remove('error');
+        submitButton.classList.add('success');
+        submitButton.textContent = "Usuário logado";
+
+    }
+
+    if (submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            submitButton.textContent = "Loading...";
+
+            fetch('https://reqres.in/api/login', { //COLOCAR API AQUI 
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response) => {
+                if (response.status !== 200) {
+                    return errorHandler();
+                }
+
+                successHandler();
+
+
+            }).catch(() => {
+                errorHandler();
+            })
+        })
+    }
+}
+
+window.onload = init;
--- assets/js/login_adm.js
+++ assets/js/login_adm.js
@@ -78,3 +78,13 @@ const init = () => {
 }
 
 window.onload = init;
+
+$(document).ready(function() {
+    $('#trigger').click(function() {
+        $('#overlay').fadeIn(300);
+    });
+
+    $('#close').click(function() {
+        $('#overlay').fadeOut(300);
+    });
+});
