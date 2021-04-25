ba11dda45b6d00c726bf376bc8600e312d9870f5
João Junior  Nebauer Schneider <55754251+joaonebauer1@users.noreply.github.com>
2021-04-25 16:21:22 -0300

    atualizaçao
    alterações no botao e no form contato

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -8,51 +8,32 @@ $(document).ready(function() {
     });
 });
 
-const init = () => {
 
-    const errorHandler = () => {
-        submitButton.classList.remove('loading');
-        submitButton.classList.remove('success');
-        submitButton.classList.add('error');
-        submitButton.textContent = "Erro, tente novamente";
+function Enviar() {
+
+    var nome = document.getElementById("nome-pessoal");
+    var telefone = document.getElementById("telefone");
+    var email_pessoal = document.getElementById("email-pessoal");
+    var cnpj = document.getElementById("cnpj");
+    var nome_empresa = document.getElementById("nome-empresa");
+    var endereco = document.getElementById("endereco");
+    var bairro = document.getElementById("bairro");
+    var cep = document.getElementById("cep");
+    var numero = document.getElementById("numero");
+    var telefone_empresa = document.getElementById("telefone-empresa");
+    var tipo_cadastro = document.getElementById("tipo");
+    var horarios = document.getElementById("horarios");
+    var imagem = document.getElementById("imagem");
+    var termos = document.getElementById("check-termo");
+
+
+    if (nome.value && telefone.value &&
+        email_pessoal.value && cnpj.value &&
+        nome_empresa.value && endereco.value && bairro.value && cep.value &&
+        numero.value && telefone_empresa.value && tipo_cadastro.value && horarios.value && imagem.value && termos.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    } else {
+        alert('Verifique se preencheu todos os campos obrigatórios')
     }
 
-    const successHandler = () => {
-        submitButton.classList.remove('loading');
-        submitButton.classList.remove('error');
-        submitButton.classList.add('success');
-        submitButton.textContent = "Usuário logado";
-
-    }
-
-    if (submitButton) {
-        submitButton.addEventListener('click', (event) => {
-            event.preventDefault();
-
-            submitButton.textContent = "Loading...";
-
-            fetch('https://reqres.in/api/login', { //COLOCAR API AQUI 
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: inputEmail.value,
-                    password: inputPassword.value,
-                })
-            }).then((response) => {
-                if (response.status !== 200) {
-                    return errorHandler();
-                }
-
-                successHandler();
-
-
-            }).catch(() => {
-                errorHandler();
-            })
-        })
-    }
 }
-
-window.onload = init;
